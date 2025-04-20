f1c98a598dd332a8b6cc7d7793d054d9d7871720
Illia Bilous <krabbys1111@gmail.com>
2025-04-20 17:43:38 +0300

    Add VPC module and root call

---
+++ terraform/aws-vpc/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.project}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.project}-private-${count.index}"
+  }
+}
